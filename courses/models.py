0760684b53f094752a8479d67483195d87b8473e
betodev90 <betodeveloper90@hotmail.com>
2018-04-20 13:11:52 -0500

    Creando los modelos en la aplicacion 'courses'

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200, help_text='Título de la asignatura. Ej: Programación')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Asignatura'
+        verbose_name_plural = 'Asignaturas'
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name='modules')          # Campo que se utiliza para relacionar con curso
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = models.PositiveIntegerField(default=0)                      # Campo para ordenar los modulos
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Módulo'
+        verbose_name_plural = 'Módulos'
+
+    def __str__(self):
+        return '{}. {}'.format(self.order, self.title)
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+    order = models.PositiveIntegerField(default=0)
+    text = models.TextField(default='', blank=True)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+    video = models.URLField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Contenido'
+        verbose_name_plural = 'Contenidos'
